56c249e79f5dde25f54401bd293957caacd2379b
rebleo <rml444@nyu.edu>
2025-10-22 08:46:54 -0400

    js exercise code

--- week06/code/theMedium.js
+++ week06/code/theMedium.js
@@ -1,5 +1,18 @@
 console.log("theMessage");
 
 // value of const variable will not change
-const theBody = document.querySelector('body')
-// let, value changes.
+//// this line passes the body element into the variable theBody
+const theBody = document.querySelector('body');
+//// this line passes the main element into the variable theMain
+let theMain = document.querySelector('main')
+// let value changes
+let theButton = document.querySelector("button");
+theButton.style.border = "10px solid black";
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+theButton.addEventListener("click", theyClicked);
+
+function theyClicked(){
+    console.log("clicked!");
+    theMain.style.backgroundColor = "pink";    
+}
