d1cd4655705de7e9838284c4684cfc6bc85addb0
rebleo <rml444@nyu.edu>
2025-10-27 11:03:18 -0400

    canvas api

--- week06/code/theMedium.js
+++ week06/code/theMedium.js
@@ -6,7 +6,7 @@ const theBody = document.querySelector('body');
 //// this line passes the main element into the variable theMain
 let theMain = document.querySelector('main')
 // let value changes
-let theButton = document.querySelector("button");
+let theButton = document.getElementById("potato");
 theButton.style.border = "10px solid black";
 
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
